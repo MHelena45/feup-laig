323a5d5ee5dc85c5f504c0dc8cc856ad31ada0ce
MHelena45 <44232337+MHelena45@users.noreply.github.com>
2019-09-27 11:59:42 +0100

    cylinder and triangle

--- Project1/main.js
+++ Project1/main.js
@@ -12,6 +12,7 @@ function getUrlVars() {
 }	 
 //Include additional files here
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js', 'MyCylinder.js',
+        'MyTriangle.js',
 
 main=function()
 {
